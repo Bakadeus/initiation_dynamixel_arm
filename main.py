622e428810ed73a87057a8f9ffc36b5e5b5e43b2
Bakadeus <tibeau031@gmail.com>
2023-09-29 15:03:32 +0200

    commentary

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ from dynamixel.interface import Interface
 from dynamixel.constants import MID_ANGLE, MIN_ANGLE
 
 
-def say_hello():
+def main():
     interface = Interface()
 
+    # It will find all motors connected to the computer with id between 1 and 18
     interface.discover()
 
     for i in interface._motors:
@@ -17,6 +18,7 @@ def say_hello():
 
     time.sleep(1)
 
+    # Get the motor with id 1, 2 and 3
     servo_01 = interface.get_servo(1)
     servo_02 = interface.get_servo(2)
     servo_03 = interface.get_servo(3)
@@ -24,134 +26,142 @@ def say_hello():
     if servo_01 and servo_02 and servo_03:
 
         commands = {
-            "coucou": lambda: coucou_command(interface),
+            "hello": lambda: hello_command(interface),
             "walkf": lambda: walkf_command(interface),
             "walkb": lambda: walkb_command(interface)
         }
 
         while True:
+            #Value for my motors to be straigth
             k = {1: 510, 2: 510, 3: 820}
+            # Send the command to the motors
             interface.update_motors(k)
+            # Wait to be sure the motors have done the command
             time.sleep(0.5)
-            print("Commandes disponibles : coucou, walkf, walkb")
-            command = input("Entrez une commande : ")
+            print("Avaible commands : hello, walkf, walkb")
+            command = input("Put a command : ")
             if command in commands:
-                commands[command]()  # Exécute la fonction associée à la commande
+                commands[command]()
             else:
-                print("Commande non reconnue")
+                print("Command not found")
 
     else:
         print("No motors founds")
 
     interface.close()
 
-
-def coucou_command(interface):
+# Command to say hello
+def hello_command(interface):
+    # Max range for my motors 3
     k = {3: 1020}
     interface.update_motors(k)
     time.sleep(0.5)
+    # Max range for my motors 1
     k = {1: 810}
     interface.update_motors(k)
     time.sleep(0.2)
     for i in range(3):
+        # Min range for my motors 1
         k = {1: 210}
         interface.update_motors(k)
         time.sleep(0.4)
+        # Max range for my motors 1
         k = {1: 810}
         interface.update_motors(k)
         time.sleep(0.4)
 
-
+# Command to walk forward like a spider
 def walkf_command(interface):
-    # Start une patte
+    # Start position
     k = {1: 510, 2: 685, 3: 960}
     interface.update_motors(k)
     time.sleep(1)
 
-    # Lève la patte
+    # Up the leg
     k = {1: 510, 2: 510, 3: 1020}
     interface.update_motors(k)
     time.sleep(0.2)
 
-    # Avance
+    # Forward
     k = {1: 360, 2: 510, 3: 1020}
     interface.update_motors(k)
     time.sleep(0.3)
 
-    # Baisse la patte
+    # Down the leg
     k = {1: 360, 2: 685, 3: 960}
     interface.update_motors(k)
     time.sleep(0.2)
 
-    # Recule
+    # Backward
     k = {1: 660, 2: 685, 3: 960}
     interface.update_motors(k)
     time.sleep(0.5)
 
     for i in range(5):
-        # Lève la patte
+        # Up the leg
         k = {1: 660, 2: 510, 3: 1020}
         interface.update_motors(k)
         time.sleep(0.2)
 
-        # Avance
+        # Forward
         k = {1: 360, 2: 510, 3: 1020}
         interface.update_motors(k)
         time.sleep(0.5)
 
-        # Baisse la patte
+        # Down the leg
         k = {1: 360, 2: 685, 3: 960}
         interface.update_motors(k)
         time.sleep(0.2)
 
-        # Recule
+        # Backward
         k = {1: 660, 2: 685, 3: 960}
         interface.update_motors(k)
         time.sleep(0.5)
 
+# Command to walk backward like a spider
 def walkb_command(interface):
-    # Start une patte
+    # Start position
     k = {1: 510, 2: 685, 3: 960}
     interface.update_motors(k)
     time.sleep(1)
 
-    # Lève la patte
+    # Up the leg
     k = {1: 510, 2: 510, 3: 960}
     interface.update_motors(k)
     time.sleep(0.2)
 
-    # Recule
+    # Backward
     k = {1: 660, 2: 510, 3: 1020}
     interface.update_motors(k)
     time.sleep(0.3)
 
-    # Baisse la patte
+    # Down the leg
     k = {1: 660, 2: 685, 3: 960}
     interface.update_motors(k)
     time.sleep(0.2)
 
-     # Avance
+    # Forward 
     k = {1: 360, 2: 685, 3: 1020}
     interface.update_motors(k)
     time.sleep(0.5)
 
     for i in range(5):
-        # Lève la patte
+        # Up the leg
         k = {1: 360, 2: 510, 3: 1020}
         interface.update_motors(k)
         time.sleep(0.2)
 
-        ## Recule
+        # Backward
         k = {1: 660, 2: 510, 3: 1020}
         interface.update_motors(k)
         time.sleep(0.5)
 
-        # Baisse la patte
+        # Down the leg
         k = {1: 660, 2: 685, 3: 960}
         interface.update_motors(k)
         time.sleep(0.2)
 
-        # Avance
+        # Forward
         k = {1: 360, 2: 685, 3: 1020}
         interface.update_motors(k)
         time.sleep(0.5)
